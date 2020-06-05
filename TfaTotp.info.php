f6200e5a6db22cb9b92fca512c403a981cef3111
Ryan Cramer <ryan@processwire.com>
2020-06-05 11:48:01 -0400

    Update TfaTotp to use encrypted secret where supported by environment (PHP 7.2+)

--- TfaTotp.info.php
+++ TfaTotp.info.php
@@ -2,7 +2,7 @@
 $info = array(
 	'title' => 'TOTP two-factor authentication ',
 	'summary' => 'For apps like Google Authenticator, Authy, and other TOTP-based authenticator applications.',
-	'version' => 1,
+	'version' => 2,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109'
 );
