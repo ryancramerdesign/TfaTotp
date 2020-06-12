436403b8060d84350fdbf7b73b6a97eace0788d0
Ryan Cramer <ryan@processwire.com>
2020-06-12 11:48:52 -0400

    Add PW 3.0.160 support for custom name, title, summary

--- TfaTotp.info.php
+++ TfaTotp.info.php
@@ -1,8 +1,8 @@
 <?php namespace ProcessWire;
 $info = array(
-	'title' => 'TOTP two-factor authentication ',
+	'title' => 'TOTP two-factor authentication',
 	'summary' => 'For apps like Google Authenticator, Authy, and other TOTP-based authenticator applications.',
-	'version' => 3,
+	'version' => 4,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109,PHP>=5.6.0'
 );
