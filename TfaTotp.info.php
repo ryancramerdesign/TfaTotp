6d65e966eb4ddf9003bd4cf9c28bdf7b023c0500
Adrian Jones <adrianbjones@gmail.com>
2025-12-05 15:22:25 +1000

    Adjust required PHP version.

--- TfaTotp.info.php
+++ TfaTotp.info.php
@@ -4,5 +4,5 @@ $info = array(
 	'summary' => 'For apps like Google Authenticator, Authy, and other TOTP-based authenticator applications.',
 	'version' => 5,
 	'singular' => true,
-	'requires' => 'ProcessWire>=3.0.109,PHP>=7.4.0'
+	'requires' => 'ProcessWire>=3.0.109,PHP>=7.1.0'
 );
